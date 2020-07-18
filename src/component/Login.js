4895ceba18fd0df601a5ae853c74c12cdd70dca9
natthapolph <birdazaz123@gmail.com>
2020-07-18 11:05:33 +0700

    ADDFILE-1

---
+++ src/component/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import auth from "../firebase";
+import "../component/css/login.css";
+const Login = ({ setSession }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await auth.signInWithEmailAndPassword(
+        username,
+        password
+      );
+      const { user } = response;
+      setSession({
+        isLogin: true,
+        currentUser: user,
+      });
+    } catch (error) {
+      setSession({
+        isLogin: false,
+        currentUser: null,
+        errorMessage: error.errorMessage,
+      });
+    }
+  };
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  return (
+    <body>
+      <header className="header-login">
+        <nav>
+          <div className="logo">
+            <h1>Ku</h1>
+          </div>
+        </nav>
+        <form id="login">
+          <h1>Login</h1>
+          <div>
+            <input type="email" placeholder="Email" onChange={handleUsername} />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={handlePassword}
+            />
+          </div>
+          <div>
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </form>
+      </header>
+    </body>
+  );
+};
+
+export default Login;
---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAhY8NV0zaw0NkdUiswhNMHnlAHtFVkdxU",
+  authDomain: "cs-project-bdf86.firebaseapp.com",
+  databaseURL: "https://cs-project-bdf86.firebaseio.com",
+  projectId: "cs-project-bdf86",
+  storageBucket: "cs-project-bdf86.appspot.com",
+  messagingSenderId: "1049486180145",
+  appId: "1:1049486180145:web:61bfee728074a4d33efa10",
+  measurementId: "G-82S94MMQ5M",
+};
+export default firebaseConfig;
