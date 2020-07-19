962469f2cbf71fa169cec1da01d845a1810fa386
TiTle-GS <tle19111@gmail.com>
2020-07-19 22:29:26 +0700

    AddFile

---
+++ hello-react/src/components/NotFound/index.js
@@ -0,0 +1,16 @@
+
+// src/components/NotFound/index.js
+import React, { PropTypes, Component } from 'react';
+import './style.css';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>404 : Not Found :(</h1>
+      </div>
+    )
+  }
+}
+
+export default NotFound;
